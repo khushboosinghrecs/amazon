5b11ddddc741336dcf8a18bb8f96f17552c4a3e3
Khushboo Singh <41840494+khushboosinghrecs@users.noreply.github.com>
2020-12-04 13:50:47 +0530

    Create repetitive_addition_of_digits.cpp

---
+++ repetitive_addition_of_digits.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+    if(n==0)
+    return 0;
+    return (n%9 ==0) ?9: n%9; 
+}
+int main() {
+	//code
+	int t;
+	cin>>t;while(t--)
+	
+	{
+	    int n;
+	    cin>>n;
+	  cout<<sum(n)<<'\n';
+	}
+	return 0;
+}

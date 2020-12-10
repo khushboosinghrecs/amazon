13edfe2ec1897515872d11bae004eb29bcfc1a30
Khushboo Singh <41840494+khushboosinghrecs@users.noreply.github.com>
2020-12-10 15:09:12 +0530

    Create rotate_mtrx_90.cpp

---
+++ rotate_mtrx_90.cpp
@@ -0,0 +1,20 @@
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    int row, temp;
+    row=matrix.size();
+    for(int i=0; i<row; i++)
+    {
+        for(int j=i+1; j<row; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for(int i=0; i<row/2; i++)
+    {
+        for(int j=0; j<row; j++)
+    {
+        swap(matrix[i][j], matrix[(row-1)-i][j]);
+    }
+    }
+}
